136d53b8bbfdf06d35e0bcad6743182f2d11d7c3
Eikaman <eirikurbi@gmail.com>
2023-09-09 13:53:09 +0100

    new function

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    float foodTime;
+
+    string cookingStatus;
 
     void Awake()
-    {}
+    { }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    // The status of the food changes
+    public void FryingFood()
+    {
+        if (foodTime <= 59)
+        {
+            baconStatus = "undercooked";
+        }
+        else if (foodTime <= 60)
+        {
+            cookingStatus = "good";
+        }
+        else if (foodTime <= 120)
+        {
+            cookingStatus = "overcooked";
+        }
     }
 
     // Update is called once per frame

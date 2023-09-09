0de8aa9984f6a30d99710455b353cd073a8e0be6
Eikaman <eirikurbi@gmail.com>
2023-09-09 21:33:35 +0100

    variable foodName

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    public float foodFryTime = 10f;
+    public float currentFryTime = 10f;
     private float currentFryTime = 0f;
-    float baconItem;
 
-    float sellEgg = 10;
+    public float foodItem;
 
+    public string foodName;
 
-    float beansItem;
+    public float optimalSell = 10.0f;
+    public float foodValue = 10.0f;
+    public float optimalTimeSeconds = 10.0f;
+    public float acceptedOffset = 2.0f;
 
-    string cookingStatus;
+    public float currentSellValue { get; private set; }
+
 
     void Awake()
     { }
@@ -44,37 +48,42 @@ public class Food : MonoBehaviour
     //     if 
     // }
 
-    public void CookTime()
-    {
+    // public void CookTime()
+    // {
 
-    }
+    // }
 
     // The status of the food changes
     public void IsFoodFried()
     {
-        if (foodFryTime <= 59)
+        if (currentFryTime <= (optimalTimeSeconds - acceptedOffset))
         {
             cookingStatus = "undercooked";
         }
-        else if (foodFryTime <= 60)
-        {
-            cookingStatus = "good";
-        }
-        else if (foodFryTime <= 120)
+        else if (currentFryTime >= (optimalTimeSeconds + acceptedOffset))
         {
             cookingStatus = "overcooked";
-
         }
+        else if (currentFryTime <= ())
     }
 
     public void fryFood()
     {
+        currentSellValue = optimalSell - optimalTimeSeconds * currentFryTime;
         currentFryTime += Time.deltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float time = Time.deltaTime;
-    }
+    // //Calculates how much money you get for the good
+    // public float calculateSellValue(float optimalSell, float acceptedOffset, float missingCookOffset)
+    // {
+    //     optimalSell = 10.0f;
+
+    //     //Formula for the food's sell value. 8-10 seconds cooking is right.
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     float time = Time.deltaTime;
+    // }
 }

bf92ddb1506d0e1f4f0d4ca8118e552f6e1a0857
Hemming <54403677+HemmingAskodan@users.noreply.github.com>
2023-09-09 16:19:40 +0100

    Changes

--- Assets/Scripts/PanSlot.cs
+++ Assets/Scripts/PanSlot.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PanSlot : MonoBehaviour
 {
+    public GameObject foodItem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float a = Time.deltaTime;
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(foodItem != null)
+        {
+            Food food = foodItem.GetComponent<Food>();
+            if(food != null)
+            {
+                float timeFried = Time.deltaTime;
+                // food.fryFood(timeFried);
+            }
+            else
+            {
+                Debug.LogError("The food in the pan doesn't have a food component");
+            }
+        }
     }
 }

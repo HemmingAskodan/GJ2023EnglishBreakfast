b4203c945bf84c6d26aff916f49b39816ded4a3b
Eikaman <eirikurbi@gmail.com>
2023-09-10 14:17:27 +0100

    menu buttons

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}

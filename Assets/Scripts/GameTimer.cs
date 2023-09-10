2fc2e3e4a95a37dc69f36b7084fd34fd2f12199b
Eikaman <eirikurbi@gmail.com>
2023-09-10 10:43:45 +0100

    changed timer

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class GameTimer : MonoBehaviour
 {
     public Text timerText;
-    public float countdownTime = 300.0f; // 5 minutes in seconds
+    public float countdownTime = 300.0f; // 300 seconds (5 minutes)
     private float currentTime;
 
     private void Start()
@@ -24,13 +24,21 @@ public class GameTimer : MonoBehaviour
         int minutes = Mathf.FloorToInt(currentTime / 60);
         int seconds = Mathf.FloorToInt(currentTime % 60);
 
-        // Display the timer in the "MM:SS" format
+        // Display the timer in "MM:SS" format
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
         // Check if the timer has reached zero
         if (currentTime <= 0.0f)
         {
-            // Call function that stops the game
+            // Call function that stops the game or performs other actions
         }
     }
+
+
+    // public float CountdownTest(float totalSeconds, float timeLimit)
+    // {
+    //     totalSeconds = 300f;
+    //     timeLimit = totalSeconds / 5;
+    //     return timeLimit;
+    // }
 }

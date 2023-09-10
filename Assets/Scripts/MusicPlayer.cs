07cb275398f117e5bc209aabf2c0fa468cb2b249
Eikaman <eirikurbi@gmail.com>
2023-09-10 16:28:00 +0100

    Music player script

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+// Makes the music play uninterrupted through scenes
+
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // If an instance already exists, destroy this one
+            Destroy(gameObject);
+        }
+    }
+}
